0accbd9db81882a7298e73a574675a15dfaaf981
Justine Daquis <justinedaquis28@gmail.com>
2022-08-01 17:01:36 +0800

    Completed overall logic, refining a bit w

---
+++ logic.js
@@ -0,0 +1,81 @@
+
+const choices = ['rock','paper','scissors']
+
+function getComputerChoice() {
+    let numChoice = Math.floor(Math.random() * choices.length)
+    return choices[numChoice]
+    // returns the computer's choice
+}
+
+function playGame(playerSelection,computerSelection) {
+    // plays one round of rock paper scissors
+    // returns a string
+    // make sure inputs are case insensitive
+    if (playerSelection === computerSelection) {
+        return "It is a draw!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lost! PAPER beats ROCK';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You won! ROCKj beats SCISSORS';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You won! PAPER beats ROCK!';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lost! SCISSORS beats PAPER!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lost! ROCK beats SCISSORS!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You won! SCISSORS beats PAPER!';
+    } else {
+        return 'you have entered the wrong input!';
+    }
+
+}
+
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (i = 0; i < 5; i++) {
+        // starts from 0, stops when condition is != true.
+        const playerChoice = prompt('Rock, Paper or Scissors?: ', '');
+        const computerChoice = getComputerChoice()
+
+        const result = playGame(playerChoice.toLowerCase(),computerChoice);
+        console.log(result);
+        if (result.slice(4,7) === 'won') {
+            
+            playerScore += 1
+            console.log('player score:'.concat(playerScore))
+            console.log('computer score:'.concat(computerScore))
+
+            if (playerScore === 3) {
+                console.log('Player has won the whole game!');
+                console.log('player score:'.concat(playerScore))
+                console.log('computer score:'.concat(computerScore))
+                break
+            }
+        } else if (result.slice(4,8) === 'lost') {
+            computerScore += 1
+            console.log('player score:'.concat(playerScore))
+            console.log('computer score:'.concat(computerScore))
+
+            if (computerScore === 3) {
+                console.log('Computer has won the whole game!');
+                console.log('player score:'.concat(playerScore))
+                console.log('computer score:'.concat(computerScore))
+                break
+            }
+
+        }
+
+        
+
+    }
+    // 5 round game
+
+}
+game()
+
+
+
